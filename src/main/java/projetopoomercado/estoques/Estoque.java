126e66cf19845ab7c8df0984293099e428cb54c1
yrnrodri <149279711+yrnrodri@users.noreply.github.com>
2023-11-05 23:27:06 -0300

    Update Estoque.java

--- src/main/java/projetopoomercado/estoques/Estoque.java
+++ src/main/java/projetopoomercado/estoques/Estoque.java
@@ -8,42 +8,46 @@ import projetopoomercado.produtos.ProdutoComestivel;
 
 
 public class Estoque implements IEstoque {
-    
+
+    //atributos
     private Vector<Produto> estoque;
     private double saldo;
-    
+
+    //construtor
     public Estoque(){
         this.estoque = new Vector<Produto>();
         this.saldo = 0.0;
       
     }
    
-        
+    //metodo para inserir produtos no estoque    
     @Override
     public void inserir(Produto produto, int quantidade){
         if(produto != null){
-                 if(!this.existe(produto.getId())){
-                produto.setQuantidade(quantidade);
-                this.estoque.add(produto);
+                 if(!this.existe(produto.getId())){ //checa se o produto não existe no estoque
+                produto.setQuantidade(quantidade);  //se não existir, seta a quantidade
+                this.estoque.add(produto);           //e coloca no vetor de estoque
             }
                  else if(this.existe(produto.getId())){
-                     produto.setQuantidade(produto.getQuantidade() + quantidade);
+                     produto.setQuantidade(produto.getQuantidade() + quantidade); //se já existe no estoque, vai so pegar a quantidade que ja tinha e adicionar a quantidade desejada
                  }
             
             }
            
     }
 
+    //metodo para procurar um produto no estoque
     @Override
     public Produto procurar(String id) {
-       for(Produto produto : estoque){
-            if(produto.getId().equals(id)){
+       for(Produto produto : estoque){ //percorre o vetor estoque
+            if(produto.getId().equals(id)){  //se algum produto dentro do estoque tiver o mesmo id do id digitado, retornará ele
                 return produto;
             }            
         }
          return null;
     }
 
+    //metodo para ver se o produto existe no estoque, mesmo funcionamento do procurar, mas esse retona um boolean
     @Override
     public boolean existe(String id) {
         for(Produto produto : estoque){
@@ -55,18 +59,20 @@ public class Estoque implements IEstoque {
         return false;
     }
 
+    //metodo para reduzir um produto do estoque
     @Override
     public void reduzir(Produto produto, int quantidade) {
             if(produto != null){
-                produto.setQuantidade(produto.getQuantidade() - quantidade);
+                produto.setQuantidade(produto.getQuantidade() - quantidade); //pega a quantidade que tinha antes, e diminui pela desejada
             }
     }
-    
+
+    //metodo para mostrar os produtos do estoque pelo tipo dele
     @Override
     public void mostrarEstoque(String tipo){
-        for(Produto produto : estoque){
-            if(produto.getTipo().equals(tipo)){
-                if(produto instanceof ProdutoComestivel){
+        for(Produto produto : estoque){ //percorre o vetor estoque
+            if(produto.getTipo().equals(tipo)){ //se o produto tiver o mesmo tipo do tipo digitado, ele será mostrado
+                if(produto instanceof ProdutoComestivel){ //caso seja comestivel, mostrará sua data de validade
                     System.out.println(((ProdutoComestivel) produto).CustomtoString());
                 }
                 else{
@@ -76,11 +82,13 @@ public class Estoque implements IEstoque {
         }
     }
 
+    //metodo para checar o saldo
     @Override
     public double verSaldo() {
        return saldo;
     }
 
+    //metodo para mudar o saldo
     @Override
     public void definirSaldo(double valor) {
        this.saldo = valor;
